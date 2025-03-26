a856bf5896b19e11582bc428ef003a78cc957dc2
Avery Vinlove <144643328+rakdosintolerant@users.noreply.github.com>
2025-03-26 14:46:10 -0600

    added deepseek's collision system

--- penguins.py
+++ penguins.py
@@ -11,6 +11,7 @@ movement = [0, 0]
 xmove = 0
 ymove = 0
 spacePressed = False
+collided = False
 
 class penguin:
     def __init__(self, rectangle, num):
@@ -24,6 +25,7 @@ class penguin:
         self.color = "red"
         self.arrow = [0, 0]
         self.clicked = False
+        self.mass = 1
     
     def setMove(self, xy):
         self.xmove = xy[0]
@@ -38,6 +40,13 @@ class penguin:
     
     def getMove(self):
         return [self.xmove, self.ymove]
+    
+    def getMass(self):
+        return self.mass
+    
+    @property
+    def id(self):
+        return self.num
     
     def getFlung(self):
         return self.flung
@@ -69,6 +78,60 @@ class penguin:
 def resetPenguins():
     for penguin in penguins:
         penguin.reset()
+
+# Function to check and resolve collisions
+def check_collision(peng1, peng2):
+        rect1 = peng1.getRectangle()
+        rect2 = peng2.getRectangle()
+    # Check if rectangles are colliding
+        # Calculate overlap in both axes
+        overlap_x = min(rect1.right - rect2.left, rect2.right - rect1.left)
+        overlap_y = min(rect1.bottom - rect2.top, rect2.bottom - rect1.top)
+        
+        # Determine the axis of least penetration
+        if overlap_x < overlap_y:
+            # Horizontal collision
+            if rect1.centerx < rect2.centerx:
+                # Rect1 is to the left of rect2
+                rect1.x = rect2.left - rect1.width
+            else:
+                # Rect1 is to the right of rect2
+                rect1.x = rect2.right
+            
+            # Calculate velocities along the collision normal
+            v1 = peng1.getMove()[0]
+            v2 = peng2.getMove()[0]
+            
+            # Calculate new velocities using conservation of momentum and energy
+            new_v1 = (.9 * peng2.getMass() * (v2 - v1) + peng1.getMass() * v1 + peng2.getMass() * v2) / (peng1.getMass() + peng2.getMass())
+            new_v2 = (.9 * peng1.getMass() * (v1 - v2) + peng1.getMass() * v1 + peng2.getMass() * v2) / (peng1.getMass() + peng2.getMass())
+
+            peng1.setMove([new_v1, peng1.getMove()[1]])
+            peng2.setMove([new_v2, peng2.getMove()[1]])
+            #rect1.vel_x = new_v1
+            #rect2.vel_x = new_v2
+            
+        else:
+            # Vertical collision
+            if rect1.centery < rect2.centery:
+                # Rect1 is above rect2
+                rect1.y = rect2.top - rect1.height
+            else:
+                # Rect1 is below rect2
+                rect1.y = rect2.bottom
+            
+            # Calculate velocities along the collision normal
+            v1 = peng1.getMove()[1]
+            v2 = peng2.getMove()[1]
+            
+            # Calculate new velocities using conservation of momentum and energy
+            new_v1 = (.9 * peng2.getMass() * (v2 - v1) + peng1.getMass() * v1 + peng2.getMass() * v2) / (peng1.getMass() + peng2.getMass())
+            new_v2 = (.9 * peng1.getMass() * (v1 - v2) + peng1.getMass() * v1 + peng2.getMass() * v2) / (peng1.getMass() + peng2.getMass())
+            
+            peng1.setMove([peng1.getMove()[0], new_v1])
+            peng2.setMove([peng2.getMove()[0], new_v2])
+            #rect1.vel_y = new_v1
+            #rect2.vel_y = new_v2
 
 penguin1 = penguin(pygame.Rect(600, 300, 50, 50), 1)
 penguin2 = penguin(pygame.Rect(300, 300, 50, 50), 2)
@@ -112,6 +175,13 @@ while running:
         if (not keys[pygame.K_SPACE]) and spacePressed:
             spacePressed = False
 
+    for penguin in penguins:
+        for otherPenguin in penguins:
+            if penguin.getRectangle().colliderect(otherPenguin.getRectangle()) and penguin.id != otherPenguin.id:
+                check_collision(penguin, otherPenguin)
+                
+        #penguin1.setMove([penguin1.getMove()[0] + penguin2.getMove()[0], penguin1.getMove()[1] + penguin2.getMove()[1]])
+        #penguin2.setMove([penguin2.getMove()[0] - penguin1.getMove()[0], penguin2.getMove()[1] - penguin1.getMove()[1]])
     # RENDER YOUR GAME HERE
     for penguin in penguins:
         if penguin.getClicked():
